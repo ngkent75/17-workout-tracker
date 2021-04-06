d7a792d8e5371d809af482b6550a450bcd5fa5a0
Nate Kent <ngkent75@gmail.com>
2021-04-05 20:42:41 -0400

    added server file and workout model, added route files but need to fill in

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+});
+
+app.use(require('./routes/api'));
+app.use(require('./routes/home-routes'));
+
+app.listen(PORT, () => {
+    console.log(`App running on  http://localhost:${PORT}`);
+});
