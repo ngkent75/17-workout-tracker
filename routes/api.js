3291dcb859941c46a67fada7f6576de86ce4d405
Nate Kent <ngkent75@gmail.com>
2021-04-05 21:16:26 -0400

    forgot to export api.js

--- routes/api.js
+++ routes/api.js
@@ -67,3 +67,5 @@ router.get('/api/workouts/range', (req, res) => {
             res.status(400).json(err);
         })
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require('path');
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
 });
 
-// routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/home-routes.js"));
+app.use(require('./routes/api'));
+app.use(require('./routes/home-routes'));
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+    console.log(`App running on  http://localhost:${PORT}`);
 });
